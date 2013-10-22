29cbd91603614cf4302d1299bfd36d4d0773d6ce
katee <hello@kate.io>
2013-10-22 19:09:45 -0400

    Accepts a file or a directory that includes .vm files.

--- vm2assembly/vm2assembly.js
+++ vm2assembly/vm2assembly.js
@@ -2,48 +2,84 @@ var _ = require('underscore');
 var Q = require('q');
 Q.longStackSupport = true;
 var fs = require('fs');
+var path = require('path');
 
 var Parser = require('./parser');
 var CodeWriter = require('./code-writer');
-
-if (process.argv.length > 2) {
-  var args = process.argv.slice(2);
-  var inFilename = args[0];
-
-  // if no outfile name is passed use the infilename but change the extension to be .asm
-  var outFilename = args[1] || inFilename.replace(/\.vm$/, '.asm');
-  // deal with case where the inFilename is not a .vm
-  if (inFilename == outFilename) {
-    throw {name: "NoOutFile", message: "Please provide a filename to write out to."};
-  }
-
-  // if the outFile exists, delete it
-  fs.exists(outFilename, function (exists) {
-    if (exists) fs.unlinkSync(outFilename);
-  });
-} else {
-  console.log("Please provide a filename to read from.");
-  process.exit();
-}
 
 var options = {
   tempStart: 5,
   staticStart: 16,
   stackStart: 256,
   heapStart: 2048,
-  debug: true
+  debug: true,
+  writeInit: false,
+  writeExit: false
 };
 
-fs.readFile(inFilename, 'utf8', function(err, data){
+if (process.argv.length > 2) {
+  var args = process.argv.slice(2);
+  var inFilename = args[0];
+} else {
+  console.log("Please provide a filename to read from.");
+  process.exit();
+}
+
+Q.nfcall(fs.stat, inFilename).then(function(inFileStats){
+  var outFilename = args[1];
+  var ends_in_vm = /\.vm$/;
+
+  if (inFileStats.isFile() && inFilename.match(ends_in_vm)) {
+    // if no outfile name is passed use the infilename but change the extension to be .asm
+    outFilename = outFilename || inFilename.replace(ends_in_vm, '.asm');
+  } else if (inFileStats.isDirectory()) {
+    // name it the folder name but with a .asm extension
+    outFilename = outFilename || path.join(inFilename, inFilename + '.asm');
+  } else {
+    throw {name: "UnknownInFile", message: "Please provide a .vm file or a directory with one or more .vm files."};
+  }
+
+  return [outFilename, inFileStats];
+}).spread(function(outFilename, inFileStats){
+  // if the outFile exists, delete it
+  fs.exists(outFilename, function (exists) {
+    if (exists) fs.unlinkSync(outFilename);
+  });
+
+  var filenames;
+  if (inFileStats.isDirectory()) {
+    // get all .vm files in the inFilename directory
+    filenames = _.chain(fs.readdirSync(inFilename))
+                .filter(function(name){return name.match(/\.vm$/);})
+                .map(function(filename){return path.join(inFilename, filename);})
+                .value();
+  } else {
+    filenames = [outFilename];
+  }
+
+  if (filenames.length < 1) {
+    throw {name: "NoSource", message: "No .vm files found in " + inFilename};
+  }
+
+  return [filenames, outFilename];
+}).spread(function(filenames, outFilename){
+  return [Q.all(filenames.map(function(filename){
+    return Q.nfcall(fs.readFile, filename, 'utf8');
+  })), outFilename];
+}).spread(function(filesContent, outFilename){
+  startParsing(filesContent.join('\n'), outFilename);
+}).done();
+
+function startParsing(data, outFilename) {
   var parser = new Parser(data);
   var codeWriter = new CodeWriter(outFilename, options);
 
-  codeWriter.writeInit();
+  if (options.writeInit) codeWriter.writeInit();
 
   _.each(parser.commands, function(command){
     var parsedCommand = Parser.parseCommand(command);
     codeWriter.write(parsedCommand);
   });
 
-  codeWriter.writeExit();
-});
+  if (options.writeExit) codeWriter.writeExit();
+}
